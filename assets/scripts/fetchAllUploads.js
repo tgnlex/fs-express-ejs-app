8a989ff0de65b5227ebfc42949e35977f77f74d7
tgnlex <alexanderwebber76@gmail.com>
2023-12-31 22:53:26 -0800

    fixed get all files route

--- assets/scripts/fetchAllUploads.js
+++ assets/scripts/fetchAllUploads.js
@@ -1,7 +1,6 @@
 const btn = document.querySelector('#fetch-btn')
 async function logFiles () {
-    const res = await fetch('http://localhost:3000/uploads/')
+    const res = await fetch('http://localhost:3000/uploads')
     console.log(res.body)
-    
 }
 btn.addEventListener('click', (logFiles))
--- src/server.js
+++ src/server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs')
 const HOST = 'localhost';
 const PORT = 3000;
 const app = express()
 const upload = multer({dest: 'uploads/'})
 
+const errMsg = `[HTTP]: An error has occurred`;
+
+async function stall(stallTime = 3000) {
+    await new Promise(resolve => setTimeout(resolve, stallTime))
+}
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}))
@@ -38,27 +44,27 @@ app.get('/signin', (req, res, next) => {
                  // ** Utility Routes ** //
 
 app.get('/status', (req, res, next) => {
-        const status = {
-            "Status":"Running"
-        };
-        const message = `[HTTP]: ${status}`
-      console.log(message)
-      res.send(status)
-    });
+  const status = {
+    "Status":"Running"
+  };
+  const message = `[HTTP]: ${status}`
+  console.log(message)
+  res.send(message)
+});
 // ######################################################## //
                 /**  Authentication Routes **/
 app.post('/signup', () => {
   
-})
+});
 
 
 app.post('/login', () => {
 
-})
+});
 
 app.get('/user', () => {
 
-})
+});
 
 
 
@@ -70,13 +76,36 @@ app.post('/profile', upload.single('avatar'), (req, res, next) => {
 });
 */
 
-app.get('/uploads', (req, res, next) => {
-  res.send('/uploads/*')
-})
+app.post('/info/all-files', (req, res, next) => {
+    fs.readdir(`uploads/`, function (err, files) {
+    if (err) {
+     return console.log(`${errMsg}`);
+    }
+    files.forEach(function (file) {
+        console.log(file)
+    })
+      res.send(files);
+
+  });
+});
 app.post('/uploads', upload.single('files'), (req, res, next) => {
-    console.log('[HTTP]: "/uploads" Endpoint Hit.' )
-    const title = req.body.filename;
-    const file = req.file;
+  console.log('[HTTP]: "/uploads" Endpoint Hit.' )
+  const title = req.body.title;     
+  const file = req.file;
+  const oldName = file.originalname;
+  const newName = `${title}.jpg`;
+    fs.rename(oldName, newName, function (err) {
+      if (err) { 
+        const uploadErr = `${errMsg} while trying to upload this file.`;
+        console.log(uploadErr);
+        console.log(err)        
+        res.send(uploadErr);
+        res.send(err);
+        res.end()
+      } else {
+        res.send(`[HTTP]: File uploaded as ${file.newName}! `)
+    }
+    });
     console.log(title)
     console.log(file)
 });
@@ -87,9 +116,9 @@ app.post('/uploads', upload.single('files'), (req, res, next) => {
 
 app.post('/search', (req, res, next) => {
     res.send() 
-})
+});
 
 
 app.listen(PORT, (req, res, next) => {
     console.log(`[HTTP]: Server running at http://${HOST}:${PORT}/` )
-})
+});
